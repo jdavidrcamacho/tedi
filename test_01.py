abe5a4384467cb40c46f27be80187daa5def11ee
jdavidrcamacho <jdavidrcamacho@gmail.com>
2018-09-20 11:46:01 +0100

    tests of the GPs and TPs

--- test_01.py
+++ test_01.py
@@ -20,6 +20,7 @@ kernel = kernels.Exponential(10,1,0.1)
 mean = None
 print(kernel)
 
+#Gaussian processes
 gpOBJ = process.GP(kernel,mean,time,y,yerr)
 print(gpOBJ.log_likelihood(kernel))
 print(gpOBJ.log_likelihood_gradient(kernel))
@@ -28,12 +29,32 @@ new_hyperparms = np.array([20.0, 3.0 ,1.0])
 kernel1 = gpOBJ.new_kernel(kernel, new_hyperparms)
 print(kernel1)
 
-mean, std, cov = gpOBJ.prediction(time = np.linspace(1,10,10))
+mean_gp, std_gp, cov_gp = gpOBJ.prediction(time = np.linspace(1,10,1000))
 
-tpOBJ = process.TP(kernel,mean,time,y,yerr)
+#Student-t processes
+tpOBJ = process.TP(kernel, 5, mean,time,y,yerr)
 print(tpOBJ.log_likelihood(kernel, 5))
 print(tpOBJ.log_likelihood_gradient(kernel, 5))
+mean_tp, std_tp, cov_tp = tpOBJ.prediction(time = np.linspace(1,10,1000))
 
+
+#plots
+f, (ax1, ax2) = plt.subplots(2, sharex=True)
+ax1.set_title(' ')
+ax1.fill_between(np.linspace(1,10,1000), mean_gp+std_gp, mean_gp-std_gp, 
+                 color="grey", alpha=0.5)
+ax1.plot(np.linspace(1,10,1000), mean_gp, "k--", alpha=1, lw=1.5)
+ax1.plot(time, y,"b.")
+ax1.set_ylabel("GPs")
+
+ax2.fill_between(np.linspace(1,10,1000), mean_tp+std_tp, mean_tp-std_tp, 
+                 color="grey", alpha=0.5)
+ax2.plot(np.linspace(1,10,1000), mean_tp, "k--", alpha=1, lw=1.5)
+ax2.plot(time, y,"b.")
+ax2.set_ylabel("TPs")
+plt.show()
+
+#samples
 plt.figure()
 for i in range(3):
     plt.plot(time, y, 'k-')
