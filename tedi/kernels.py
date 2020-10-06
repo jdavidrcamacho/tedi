1c27239a4c5c92ab2aaf181e00c850c777f67e69
jdavidrcamacho <joao.camacho@astro.up.pt>
2020-10-06 16:10:33 +0100

    woriking

--- tedi/kernels.py
+++ tedi/kernels.py
@@ -904,106 +904,6 @@ class dRQP_dwn(RQP):
         raise NotImplementedError
 
 
-class varRQP(kernel):
-    """
-    WARNING: EXPERIMENTAL KERNEL
-    Definition of the product between the exponential sine squared kernel 
-    and the rational quadratic kernel that we called RQP kernel.
-    If I am thinking this correctly then this kernel should tend to the
-    QuasiPeriodic kernel as alpha increases, although I am not sure if we can
-    say that it tends to the QuasiPeriodic kernel as alpha tends to infinity.
-    
-    Parameters
-    ----------
-    amplitude: float
-        Amplitude of the kernel
-    ell_e and ell_p: float
-        Aperiodic and periodic lenght scales
-    var: float
-        var = eta2**(-4)/alpha
-    alpha: NOT IMPLEMENTED 
-        alpha of the rational quadratic kernel
-    P: float
-        Periodic repetitions of the kernel
-    wn: float
-        White noise amplitude
-    """
-    def __init__(self, amplitude, var, ell_e, P, ell_p, wn):
-        super(varRQP, self).__init__(amplitude, var, ell_e, P, ell_p, wn)
-        self.amplitude = amplitude
-        self.var = var
-        self.ell_e = ell_e
-        self.P = P
-        self.ell_p = ell_p
-        self.wn = wn
-        self.type = 'non-stationary and anisotropic'
-        self.derivatives = 0    #number of derivatives in this kernel
-        self.params_number = 6  #number of hyperparameters
-        self.alpha = self.ell_e**(-4) / self.var
-        
-    def __call__(self, r):
-        try:
-            #because of numpy issues
-            a = exp(- 2*sine(pi*np.abs(r)/self.P)**2 / self.ell_p**2)
-            b = (1+ r**2/ (2*self.alpha*self.ell_e**2))#**self.alpha
-            c = self.wn**2 * np.diag(np.diag(np.ones_like(r)))
-            return self.amplitude**2 * a / (np.sign(b) * (np.abs(b)) ** self.alpha) + c
-        except ValueError:
-            a = exp(- 2*sine(pi*np.abs(r)/self.P)**2 / self.ell_p**2)
-            b = (1+ r**2/ (2*self.alpha*self.ell_e**2))#**self.alpha
-            return self.amplitude**2 * a / (np.sign(b) * (np.abs(b)) ** self.alpha)
-            
-            
-class varRQP2(kernel):
-    """
-    WARNING: EXPERIMENTAL KERNEL
-    Definition of the product between the exponential sine squared kernel 
-    and the rational quadratic kernel that we called RQP kernel.
-    If I am thinking this correctly then this kernel should tend to the
-    QuasiPeriodic kernel as alpha increases, although I am not sure if we can
-    say that it tends to the QuasiPeriodic kernel as alpha tends to infinity.
-    
-    Parameters
-    ----------
-    amplitude: float
-        Amplitude of the kernel
-    ell_e and ell_p: float
-        Aperiodic and periodic lenght scales
-    var: float
-        var = shape * scale**2 = alpha * eta2**4 
-    alpha: NOT IMPLEMENTED 
-        alpha of the rational quadratic kernel
-    P: float
-        Periodic repetitions of the kernel
-    wn: float
-        White noise amplitude
-    """
-    def __init__(self, amplitude, var, ell_e, P, ell_p, wn):
-        super(varRQP2, self).__init__(amplitude, var, ell_e, P, ell_p, wn)
-        self.amplitude = amplitude
-        self.var = var
-        self.ell_e = ell_e
-        self.P = P
-        self.ell_p = ell_p
-        self.wn = wn
-        self.type = 'non-stationary and anisotropic'
-        self.derivatives = 0    #number of derivatives in this kernel
-        self.params_number = 6  #number of hyperparameters
-        self.alpha = self.var / self.ell_e**4
-        
-    def __call__(self, r):
-        try:
-            #because of numpy issues
-            a = exp(- 2*sine(pi*np.abs(r)/self.P)**2 / self.ell_p**2)
-            b = (1+ r**2/ (2*self.alpha*self.ell_e**2))#**self.alpha
-            c = self.wn**2 * np.diag(np.diag(np.ones_like(r)))
-            return self.amplitude**2 * a / (np.sign(b) * (np.abs(b)) ** self.alpha) + c
-        except ValueError:
-            a = exp(- 2*sine(pi*np.abs(r)/self.P)**2 / self.ell_p**2)
-            b = (1+ r**2/ (2*self.alpha*self.ell_e**2))#**self.alpha
-            return self.amplitude**2 * a / (np.sign(b) * (np.abs(b)) ** self.alpha)
-
-
 ###############################################################################
 class PiecewiseSE(kernel):
     """
@@ -1021,43 +921,15 @@ class PiecewiseSE(kernel):
         self.derivatives = 0    #number of derivatives in this kernel
         self.params_number = 0    #number of hyperparameters
     def __call__(self, r):
-        evaluate = np.abs(r / self.eta3/2)
         SE_term = self.eta1**2 + exp(-0.5 * r**2 / self.eta2**2)
+        #print('SE:\n',SE_term, '\n')
         piecewise = (3*r + 1) * (1 - r)**3
-        kern = SE_term*piecewise
-        kern = np.where(evaluate>1, 0, kern)
-        return kern
+        #print('piecewise:\n', piecewise, '\n')
+        #print('r:\n', np.abs(r)/(0.5*self.eta3), '\n')
+        k = SE_term*piecewise
+        k = np.where(np.abs(r)>0.5*self.eta3, 0, k)
+        #print('piecewise:\n', piecewise, '\n')
+        #k = SE_term*piecewise
+        #print('final:\n', k, '\n')
+        return k
 
-
-##### supa kernel #############################################################
-class supa(kernel):
-    """
-    WARNING: EXPERIMENTAL KERNEL
-    
-    Is it a bird? Is it a plane? NO... It's the supa kernel
-    """
-    def __init__(self, QP1, QP2, QP3, QP4, SE1, SE2, wn):
-        super(supa, self).__init__(QP1, QP2, QP3, QP4, SE1, SE2, wn)
-        self.QP1 = QP1
-        self.QP2 = QP1
-        self.QP3 = QP3
-        self.QP4 = QP4
-        self.SE1 = SE1
-        self.SE2 = SE2
-        self.wn = wn
-        self.type = 'unknown'
-        self.derivatives = 'not implemented'
-        self.params_number = 7 #number of hyperparameters
-        self.halfEta3 = self.QP3/2
-        
-    def __call__(self, r):
-        try:
-            evaluate = np.abs(r / self.halfEta3)
-            rr = np.where(evaluate>1, 0, evaluate)
-            QP_term = self.QP1**2 * exp(- 2*sine(pi*np.abs(r)/self.QP3)**2 /self.QP4**2 - r**2/(2*self.QP2**2))
-            SE_term = self.SE1**2 + exp(-0.5 * r**2 / self.SE2**2)
-            Piecewise_term = (3*rr + 1) * (1 - rr)**3
-            WN_term = self.wn**2*np.diag(np.diag(np.ones_like(r)))
-            return QP_term + SE_term*Piecewise_term + WN_term
-        except ValueError:
-            return QP_term + SE_term*Piecewise_term 
