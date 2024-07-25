211408805877eefeb9996da5d1b05157a69ce7a1
jdavidrcamacho <jdavidrcamacho@gmail.com>
2024-07-25 20:56:40 +0100

    fix mean functions

--- examples/calculate_likelihood.py
+++ examples/calculate_likelihood.py
@@ -13,14 +13,14 @@ yerr = np.random.uniform(0, 0.5, time.size)
 
 
 kernel = kernels.Exponential(10, 1) + kernels.WhiteNoise(0.1)
-mean = means.Constant(0)
+mean = means.Cosine(1, 10, 0, 0)
 print(kernel)
 
 # Gaussian processes
 gpOBJ = process.GP(kernel, mean, time, y, yerr)
 gp_loglike = gpOBJ.log_likelihood(kernel)
 print(f"GP log-likelihood = {gp_loglike}")
-assert gp_loglike == -134.7964846941749
+assert gp_loglike == -134.6115049347123
 
 # Student-t processes
 tpOBJ = process.TP(kernel, 5, mean, time, y, yerr)
@@ -33,10 +33,10 @@ kernel = kernels.Matern32(10, 1) + kernels.WhiteNoise(0.1)
 gpOBJ = process.GP(kernel, mean, time, y, yerr)
 gp_loglike = gpOBJ.log_likelihood(kernel)
 print(f"GP log-likelihood = {gp_loglike}")
-assert gp_loglike == -88.89074571448118
+assert gp_loglike == -88.75761546043923
 
 kernel = kernels.PiecewiseSE(1, 2, 10) + kernels.WhiteNoise(0.1)
 gpOBJ = process.GP(kernel, mean, time, y, yerr)
 gp_loglike = gpOBJ.log_likelihood(kernel)
 print(f"GP log-likelihood = {gp_loglike}")
-assert gp_loglike == -199.4120846074714
+assert gp_loglike == -190.0126293943043
--- src/tedi/means.py
+++ src/tedi/means.py
@@ -1,270 +1,362 @@
-"""
-Mean functions
-"""
-
-from functools import wraps
+"""Mean functions for Gaussian or Student-t processes regression."""
 
 import numpy as np
 
+from .utils.means import MeanModel, array_input
+
+pi, exp, sine, cosine, sqrt = np.pi, np.exp, np.sin, np.cos, np.sqrt
 __all__ = ["Constant", "Linear", "Parabola", "Cubic", "Keplerian", "UdHO"]
 
 
-def array_input(f):
-    """Decorator to provide the __call__ methods with an array"""
-
-    @wraps(f)
-    def wrapped(self, t):
-        t = np.atleast_1d(t)
-        r = f(self, t)
-        return r
-
-    return wrapped
-
-
-class MeanModel(object):
-    _parsize = 0
-
-    def __init__(self, *pars):
-        self.pars = list(pars)
-        # np.array(pars, dtype=float)
-
-    def __repr__(self):
-        """Representation of each instance"""
-        return "{0}({1})".format(
-            self.__class__.__name__, ", ".join(map(str, self.pars))
-        )
-
-    @classmethod
-    def initialize(cls):
-        """Initialize instance, setting all parameters to 0."""
-        return cls(*([0.0] * cls._parsize))
-
-    def __add__(self, b):
-        return Sum(self, b)
-
-    def __radd__(self, b):
-        return self.__add__(b)
-
-
-class Sum(MeanModel):
-    """Sum of two mean functions"""
-
-    def __init__(self, m1, m2):
-        self.m1, self.m2 = m1, m2
-
-    @property
-    def _parsize(self):
-        return self.m1._parsize + self.m2._parsize
-
-    @property
-    def pars(self):
-        return self.m1.pars + self.m2.pars
-
-    def initialize(self):
-        return
-
-    def __repr__(self):
-        return "{0} + {1}".format(self.m1, self.m2)
-
-    @array_input
-    def __call__(self, t):
-        return self.m1(t) + self.m2(t)
-
-
-##### Constant mean ###########################################################
 class Constant(MeanModel):
-    """A constant offset mean function"""
+    """
+    Constant offset mean function.
+
+    Attributes:
+        _parsize (int): The number of parameters in the mean model.
+    """
 
     _parsize = 1
 
-    def __init__(self, c):
+    def __init__(self, c: float) -> None:
+        """
+        Initializes the constant mean function with a given offset.
+
+        Args:
+            c (float): The constant offset value.
+        """
         super(Constant, self).__init__(c)
 
     @array_input
-    def __call__(self, t):
+    def __call__(self, t: np.ndarray) -> np.ndarray:
+        """
+        Evaluates the constant mean function at the given input.
+
+        Args:
+            t (np.ndarray): Input data.
+
+        Returns:
+            np.ndarray: An array with the constant value.
+        """
         return np.full(t.shape, self.pars[0])
 
 
-##### Linear mean #############################################################
 class Linear(MeanModel):
-    """Linear mean; m(t) = slope * t + intercept"""
+    """
+    Linear mean function
+
+    This class models a mean function of the form:
+        m(t) = slope * t + intercept.
+
+    Attributes:
+        _parsize (int): The number of parameters in the mean model.
+    """
 
     _parsize = 2
 
-    def __init__(self, slope, intercept):
+    def __init__(self, slope: float, intercept: float) -> None:
+        """
+        Initializes the linear mean function with a given slope and intercept.
+
+        Args:
+            slope (float): The slope of the linear function.
+            intercept (float): The intercept of the linear function.
+        """
         super(Linear, self).__init__(slope, intercept)
 
     @array_input
-    def __call__(self, t):
-        tmean = t.mean()
-        return self.pars[0] * (t - tmean) + np.full(t.shape, self.pars[1])
+    def __call__(self, t: np.ndarray) -> np.ndarray:
+        """
+        Evaluates the linear mean function at the given input.
+
+        Args:
+            t (np.ndarray): Input data.
+
+        Returns:
+            np.ndarray: An array with the linear mean values.
+        """
+        t_mean = t.mean()
+        return self.pars[0] * (t - t_mean) + np.full(t.shape, self.pars[1])
 
 
-##### Parabolic mean ##########################################################
 class Parabola(MeanModel):
     """
-    2nd degree polynomial mean
-    m(t) = quad * t**2 + slope * t + intercept"""
+    Second degree polynomial mean function.
+
+    This class models a mean function of the form:
+        m(t) = quad * t**2 + slope * t + intercept
+
+    Attributes:
+        _parsize (int): The number of parameters in the mean model.
+    """
 
     _parsize = 3
 
-    def __init__(self, quad, slope, intercept):
+    def __init__(self, quad: float, slope: float, intercept: float) -> None:
+        """
+        Initializes the parabolic mean function.
+
+        Args:
+            quad (float): The coefficient of the quadratic term.
+            slope (float): The coefficient of the linear term.
+            intercept (float): The intercept of the polynomial.
+        """
         super(Parabola, self).__init__(quad, slope, intercept)
 
     @array_input
-    def __call__(self, t):
+    def __call__(self, t: np.ndarray) -> np.ndarray:
+        """
+        Evaluates the parabolic mean function at the given input.
+
+        Args:
+            t (np.ndarray): Input data.
+
+        Returns:
+            np.ndarray: An array with the parabolic mean values.
+        """
         return np.polyval(self.pars, t)
 
 
-##### Cubic mean ##############################################################
 class Cubic(MeanModel):
     """
-    3rd degree polynomial mean
-    m(t) = cub*t**3 + quad*t**2 + slope*t + intercept"""
+    Third degree polynomial mean function.
+
+    This class models a mean function of the form:
+        m(t) = cub * t**3 + quad * t**2 + slope * t + intercept
+
+    Attributes:
+        _parsize (int): The number of parameters in the mean model.
+    """
 
     _parsize = 4
 
-    def __init__(self, cub, quad, slope, intercept):
+    def __init__(
+        self, cub: float, quad: float, slope: float, intercept: float
+    ) -> None:  # NOQA
+        """
+        Initializes the cubic mean function.
+
+        Args:
+            cub (float): The coefficient of the cubic term.
+            quad (float): The coefficient of the quadratic term.
+            slope (float): The coefficient of the linear term.
+            intercept (float): The intercept of the polynomial.
+        """
         super(Cubic, self).__init__(cub, quad, slope, intercept)
 
     @array_input
-    def __call__(self, t):
+    def __call__(self, t: np.ndarray) -> np.ndarray:
+        """
+        Evaluates the cubic mean function at the given input.
+
+        Args:
+            t (np.ndarray): Input data.
+
+        Returns:
+            np.ndarray: An array with the cubic mean values.
+        """
         return np.polyval(self.pars, t)
 
 
-##### Sinusoidal means ########################################################
 class Sine(MeanModel):
     """
-    Sinusoidal mean
-    m(t) = amplitude**2*sine((2*pi*t/P)+phase) + displacement"""
+    Sinusoidal mean function.
 
-    _parsize = 3
+    This class models a mean function of the form:
+        m(t) = amplitude * sin((2 * pi * t / p) + phase) + displacement
 
-    def __init__(self, amp, P, phi, D):
-        super(Sine, self).__init__(amp, P, phi, D)
+    Attributes:
+        _parsize (int): The number of parameters in the mean model.
+    """
+
+    _parsize = 4
+
+    def __init__(self, amp: float, p: float, phi: float, d: float) -> None:
+        """
+        Initializes the sinusoidal mean function.
+
+        Args:
+            amp (float): The amplitude of the sine wave.
+            p (float): The period of the sine wave.
+            phi (float): The phase shift of the sine wave.
+            d (float): The displacement of the sine wave.
+        """
+        super(Sine, self).__init__(amp, p, phi, d)
 
     @array_input
-    def __call__(self, t):
+    def __call__(self, t: np.ndarray) -> np.ndarray:
+        """
+        Evaluates the sinusoidal mean function at the given input.
+
+        Args:
+            t (np.ndarray): Input data.
+
+        Returns:
+            np.ndarray: An array with the sinusoidal mean values.
+        """
         return (
-            self.pars[0] * np.sin((2 * np.pi * t / self.pars[1]) + self.pars[2])
+            self.pars[0] * np.sin((2 * np.pi * t / self.pars[1]) + self.pars[2])  # NOQA
             + self.pars[3]
         )
 
 
 class Cosine(MeanModel):
     """
-    Cosine mean
-    m(t) = amplitude**2*cosine((2*pi*t/P)+phase) + displacement"""
+    Cosine mean function.
 
-    _parsize = 3
+    This class models a mean function of the form:
+        m(t) = amplitude^2 * cos((2 * pi * t / P) + phase) + displacement
 
-    def __init__(self, amp, P, phi, D):
-        super(Cosine, self).__init__(amp, P, phi, D)
-
-    @array_input
-    def __call__(self, t):
-        return (
-            self.pars[0] ** 2 * np.cos((2 * np.pi * t / self.pars[1]) + self.pars[2])
-            + self.pars[3]
-        )
-
-
-##### Keplerian means #########################################################
-class Keplerian(MeanModel):
-    """
-    Keplerian mean function adapted from radvel
-    tan[phi(t) / 2 ] = sqrt(1+e / 1-e) * tan[E(t) / 2] = true anomaly
-    E(t) - e*sin[E(t)] = M(t) = eccentric anomaly
-    M(t) = (2*pi*t/tau) + M0 = Mean anomaly
-
-    Parameters
-    ----------
-    P: float
-        Period in days
-    e: float
-        Eccentricity
-    K: float
-        RV amplitude in m/s
-    w: float
-        Longitude of the periastron
-    phi: float
-        Orbital phase
-    offset: float
-        Offset
-
-    Returns
-    -------
-    RV: array
-        RV signal generated = K[cos(w+v) + e*cos(w)] + offset
-    """
-
-    _parsize = 5
-
-    def __init__(self, p, k, e, w, phi, offset):
-        super(Keplerian, self).__init__(p, k, e, w, phi, offset)
-
-    @array_input
-    def __call__(self, t):
-        p, k, e, w, phi, offset = self.pars
-        T0 = t[0] - (p * phi) / (2.0 * np.pi)
-        M0 = 2 * np.pi * (t - T0) / p  # first guess at M
-        E0 = M0 + e * np.sin(M0) + 0.5 * (e**2) * np.sin(2 * M0)  # first guess at E
-        M1 = E0 - e * np.sin(E0) - M0  # goes to zero when converges
-        criteria = 1e-10
-        convd = np.where(np.abs(M1) > criteria)[0]  # which indices have not converged
-        nd = len(convd)  # number of unconverged elements
-        count = 0
-        while nd > 0:
-            count += 1
-            E = E0
-            M1p = 1 - e * np.cos(E)
-            M1pp = e * np.sin(E)
-            M1ppp = 1 - M1p
-            d1 = -M1 / M1p
-            d2 = -M1 / (M1p + d1 * M1pp / 2.0)
-            d3 = -M1 / (M1p + d2 * M1pp / 2.0 + d2 * d2 * M1ppp / 6.0)
-            E = E + d3
-            E0 = E
-            M0 = E0 - e * np.sin(E0)
-            M1 = E0 - e * np.sin(E0) - M0
-            convergence_criteria = np.abs(M1) > criteria
-            nd = np.sum(convergence_criteria is True)
-        nu = 2 * np.arctan(np.sqrt((1 + e) / (1 - e)) * np.tan(E0 / 2))
-        RV = k * (e * np.cos(w) + np.cos(w + nu)) + offset
-        return RV
-
-
-##### Underdamped harmonic oscillator mean ####################################
-class UdHO(MeanModel):
-    """
-    Underdamped harmonic oscillator mean
-    m(t) = A*exp(-b*t)*cos(w*t+phi)
-
-    Parameters
-    ----------
-    A: float
-        Kinda of an amplitude
-    b: float
-        Damping coefficient
-    w: float
-        Kinda of an angular frequency, w**2 = sqrt(w0**2 - b**2), where w0 is
-        the angular frequency
-    phi: float
-        Phase, determines the starting point of the "wave"
+    Attributes:
+        _parsize (int): The number of parameters in the mean model.
     """
 
     _parsize = 4
 
-    def __init__(self, A, b, w, phi):
-        super(UdHO, self).__init__(A, b, w, phi)
+    def __init__(self, amp: float, p: float, phi: float, d: float) -> None:
+        """
+        Initializes the cosine mean function.
+
+        Args:
+            amp (float): The amplitude of the cosine wave.
+            p (float): The period of the cosine wave.
+            phi (float): The phase shift of the cosine wave.
+            d (float): The displacement of the cosine wave.
+        """
+        super(Cosine, self).__init__(amp, p, phi, d)
 
     @array_input
-    def __call__(self, t):
+    def __call__(self, t: np.ndarray) -> np.ndarray:
+        """
+        Evaluates the cosine mean function at the given input.
+
+        Args:
+            t (np.ndarray): Input data.
+
+        Returns:
+            np.ndarray: An array with the cosine mean values.
+        """
         return (
             self.pars[0] ** 2
-            * np.exp(-self.pars[1] * t)
-            * np.cos(self.pars[2] * t + self.pars[3])
+            * np.cos((2 * np.pi * t / self.pars[1]) + self.pars[2])  # NOQA
+            + self.pars[3]
         )
 
 
-### END
+class Keplerian(MeanModel):
+    """
+    Keplerian mean function for modeling radial velocity (RV) signals.
+
+    This class adapts the Keplerian model for RV signals, using parameters
+    related to orbital mechanics. It calculates the RV signal based on the
+    provided parameters.
+
+    The mathematical model used is:
+        tan[phi(t) / 2] = sqrt(1 + ecc) / sqrt(1 - ecc) * tan[E(t) / 2]
+        E(t) - ecc * sin[E(t)] = M(t) = eccentric anomaly
+        M(t) = (2 * pi * t / P) + M0 = mean anomaly
+        RV = K * [cos(w + nu) + ecc * cos(w)] + offset
+    where:
+        nu = 2 * atan(sqrt((1 + ecc) / (1 - ecc)) * tan(E(t) / 2))
+
+    Attributes:
+        _parsize (int): Number of parameters in the Keplerian model (5).
+
+    Parameters:
+        p (float): Period in days.
+        ecc (float): Eccentricity.
+        k (float): RV amplitude in m/s.
+        w (float): Longitude of the periastron.
+        phi (float): Orbital phase.
+        offset (float): Offset.
+
+    Returns:
+        np.ndarray: RV signal computed from the model.
+    """
+
+    _parsize = 6
+
+    _parsize = 5
+
+    def __init__(self, p, k, ecc, w, phi, offset) -> None:
+        super(Keplerian, self).__init__(p, k, ecc, w, phi, offset)
+
+    def __call__(self, t):
+        p, k, ecc, w, phi, offset = self.pars
+        t0 = t[0] - (p * phi) / (2.0 * np.pi)
+        m0 = 2 * np.pi * (t - t0) / p  # first guess at M and E
+        e0 = m0 + ecc * np.sin(m0) + 0.5 * (ecc**2) * np.sin(2 * m0)
+        m1 = e0 - ecc * np.sin(e0) - m0  # goes to zero when converges
+        criteria = 1e-10
+        convd = np.where(np.abs(m1) > criteria)[0]  # indices not converged
+        nd = len(convd)  # number of unconverged elements
+        count = 0
+        while nd > 0:
+            count += 1
+            e = e0
+            m1p = 1 - ecc * np.cos(e)
+            m1pp = ecc * np.sin(e)
+            m1ppp = 1 - m1p
+            d1 = -m1 / m1p
+            d2 = -m1 / (m1p + d1 * m1pp / 2.0)
+            d3 = -m1 / (m1p + d2 * m1pp / 2.0 + d2 * d2 * m1ppp / 6.0)
+            e = e + d3
+            e0 = e
+            m0 = e0 - ecc * np.sin(e0)
+            m1 = e0 - ecc * np.sin(e0) - m0
+            convergence_criteria = np.abs(m1) > criteria
+            nd = np.sum(convergence_criteria is True)
+        nu = 2 * np.arctan(np.sqrt((1 + ecc) / (1 - ecc)) * np.tan(e / 2))
+        rv = k * (ecc * np.cos(w) + np.cos(w + nu)) + offset
+        return rv
+
+
+class UdHO(MeanModel):
+    """
+    Underdamped Harmonic Oscillator mean function.
+
+    This class models a mean function based on an underdamped harmonic
+    oscillator:
+        m(t) = a * exp(-b * t) * cos(w * t + phi)
+
+    Attributes:
+        _parsize (int): Number of parameters in the model.
+
+    Parameters:
+        a (float): Amplitude-like parameter that scales the oscillation.
+        b (float): Damping coefficient, controls the exponential decay.
+        w (float): Angular frequency of the oscillation.
+        phi (float): Phase shift, determines the initial phase of the
+            oscillation.
+    """
+
+    _parsize = 4
+
+    def __init__(self, a: float, b: float, w: float, phi: float) -> None:
+        """
+        Initializes the Underdamped Harmonic Oscillator mean function.
+
+        Args:
+            a (float): Amplitude-like parameter that scales the oscillation.
+            b (float): Damping coefficient, controls the exponential decay.
+            w (float): Angular frequency of the oscillation.
+            phi (float): Phase shift, determines the initial phase of the
+                oscillation.
+        """
+        super(UdHO, self).__init__(a, b, w, phi)
+
+    _parsize = 4
+
+    @array_input
+    def __call__(self, t: np.ndarray) -> np.ndarray:
+        """
+        Computes the mean function for the given time array.
+
+        Args:
+            t (np.ndarray): Array of time values.
+
+        Returns:
+            np.ndarray: Computed mean values based on the model.
+        """
+        a, b, w, phi = self.pars
+        return a**2 * np.exp(-b * t) * np.cos(w * t + phi)
