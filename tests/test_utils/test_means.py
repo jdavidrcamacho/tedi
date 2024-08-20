36307d25e288c5d387c2518dc77d9a54261ac1ca
joao.camacho <joao.camacho@visionspace.com>
2024-08-20 15:51:58 +0100

    add tests for utils/means

--- tests/test_utils/test_means.py
+++ tests/test_utils/test_means.py
@@ -3,19 +3,19 @@ import numpy as np
 from src.tedi.utils.means import MeanModel, Product, Sum, array_input
 
 
-def test_mean_model_initialization() -> None:
+def test_MeanModel() -> None:
     model = MeanModel(1.0, 2.0)
     assert model.pars == [1.0, 2.0], "MeanModel initialization failed."
     assert repr(model) == "MeanModel(1.0, 2.0)", "MeanModel repr failed."
 
 
-def test_mean_model_initialize() -> None:
+def test_MeanModel_pars() -> None:
     MeanModel._parsize = 2
     model = MeanModel.initialize()
     assert model.pars == [0.0, 0.0], "MeanModel initialize failed."
 
 
-def test_sum_initialization() -> None:
+def test_Sum() -> None:
     model1 = MeanModel(1.0)
     model2 = MeanModel(2.0)
     sum_model = model1 + model2
@@ -23,7 +23,7 @@ def test_sum_initialization() -> None:
     assert sum_model.pars == [1.0, 2.0], "Sum pars failed."
 
 
-def test_sum_repr() -> None:
+def test_Sum_repr() -> None:
     model1 = MeanModel(1.0)
     model2 = MeanModel(2.0)
     sum_model = model1 + model2
@@ -32,7 +32,7 @@ def test_sum_repr() -> None:
     ), "Sum repr failed."  # NOQA
 
 
-def test_sum_call() -> None:
+def test_Sum_call() -> None:
     class MockMean(MeanModel):
         @array_input
         def __call__(self, t):
@@ -48,7 +48,7 @@ def test_sum_call() -> None:
     assert np.allclose(result, expected_result), "Sum call failed."
 
 
-def test_product_initialization() -> None:
+def test_Product() -> None:
     model1 = MeanModel(1.0)
     model2 = MeanModel(2.0)
     product_model = model1 * model2
@@ -56,7 +56,7 @@ def test_product_initialization() -> None:
     assert product_model.pars == [1.0, 2.0], "Product pars failed."
 
 
-def test_product_repr() -> None:
+def test_Product_repr() -> None:
     model1 = MeanModel(1.0)
     model2 = MeanModel(2.0)
     product_model = model1 * model2
@@ -65,7 +65,7 @@ def test_product_repr() -> None:
     ), "Product repr failed."
 
 
-def test_product_call() -> None:
+def test_Product_call() -> None:
     class MockMean(MeanModel):
         @array_input
         def __call__(self, t):
